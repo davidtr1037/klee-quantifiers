98a5dc051805f3f0834ffcda6239b039495e1c4c
David Trabish <davidtr1037@gmail.com>
2021-04-07 15:13:11 +0300

    adding files

---
+++ lib/Core/PatternExtraction.cpp
@@ -0,0 +1,5 @@
+#include "PatternExtraction.h"
+
+void klee::extractPatterns(ExecTree &t, PatternMatch &matches) {
+
+}
---
+++ lib/Core/PatternExtraction.h
@@ -0,0 +1,23 @@
+#ifndef KLEE_PATERN_EXTRACTION_H
+#define KLEE_PATERN_EXTRACTION_H
+
+#include "ExecTree.h"
+#include "Pattern.h"
+
+namespace klee {
+
+struct StateMatch {
+  /* TODO: add state? */
+  unsigned count;
+};
+
+struct PatternMatch {
+  Pattern p;
+  std::vector<StateMatch> matches;
+};
+
+void extractPatterns(ExecTree &t, PatternMatch &matches);
+
+}
+
+#endif
