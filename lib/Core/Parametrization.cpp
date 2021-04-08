ce5ac24741e76d0c55c35af8354a7e5d07343fa0
David Trabish <davidtr1037@gmail.com>
2021-04-08 11:41:57 +0300

    updating

--- lib/Core/Parametrization.cpp
+++ lib/Core/Parametrization.cpp
@@ -1,5 +1,19 @@
 #include "Parametrization.h"
+#include "PatternExtraction.h"
 
+#include <vector>
 
 using namespace llvm;
 using namespace klee;
+
+void klee::extractEquationsForCore(ExecTree &t,
+                                   PatternMatch &pm,
+                                   SMTEquation &eqs) {
+
+}
+
+void klee::extractEquationsForSuffix(ExecTree &t,
+                                     PatternMatch &pm,
+                                     SMTEquation &eqs) {
+
+}
--- lib/Core/Parametrization.h
+++ lib/Core/Parametrization.h
@@ -1,7 +1,12 @@
 #ifndef KLEE_PARAMETRIZATION_H
 #define KLEE_PARAMETRIZATION_H
 
+#include "PatternExtraction.h"
+#include "ExecTree.h"
+
 #include <klee/Expr/Expr.h>
+
+#include <vector>
 
 namespace klee {
 
@@ -14,6 +19,14 @@ struct SMTEquation {
   ref<Expr> e;
   unsigned k;
 };
+
+void extractEquationsForCore(ExecTree &t,
+                             PatternMatch &pm,
+                             SMTEquation &eqs);
+
+void extractEquationsForSuffix(ExecTree &t,
+                               PatternMatch &pm,
+                               SMTEquation &eqs);
 
 }
 
