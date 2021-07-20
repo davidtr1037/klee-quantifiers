547c590ce4ffbbdd48751120f0eead4ae50631dd
David Trabish <davidtr1037@gmail.com>
2021-07-20 14:38:32 +0300

    adding files

---
+++ lib/Core/LivenessAnalysis.cpp
@@ -0,0 +1,146 @@
+#include <list>
+#include <set>
+#include <map>
+
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/Instruction.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/IR/Constants.h>
+#include <llvm/IR/InstIterator.h>
+#include <llvm/IR/CFG.h>
+#include <llvm/Support/raw_ostream.h>
+
+#include "LivenessAnalysis.h"
+
+
+using namespace llvm;
+using namespace std;
+
+namespace klee {
+
+void LivenessAnalysis::analyze(Function *f) {
+  LiveSet live_in;
+  LiveSet live_out;
+  bool changed;
+
+  do {
+    changed = runIteration(f, live_in, live_out);
+  } while (changed);
+}
+
+bool LivenessAnalysis::runIteration(Function *f,
+                                    LiveSet &live_in,
+                                    LiveSet &live_out) {
+  list<Instruction *> worklist;
+  set<Instruction *> visited;
+  bool changed = false;
+
+  for (BasicBlock &bb : *f) {
+    for (Instruction &inst : bb) {
+      if (isa<ReturnInst>(&inst)) {
+        worklist.push_back(&inst);
+      }
+    }
+  }
+
+  while (!worklist.empty()) {
+    Instruction *inst = worklist.front();
+    worklist.pop_front();
+
+    if (visited.find(inst) != visited.end()) {
+      continue;
+    }
+
+    errs() << "HANDLE " << *inst << "\n";
+    set<StringRef> toKill, toGen;
+    gen(inst, toGen);
+    kill(inst, toKill);
+
+    /* live-out */
+    std::vector<Instruction *> successors;
+    if (isa<TerminatorInst>(inst)) {
+      TerminatorInst *termInst = dyn_cast<TerminatorInst>(inst);
+      for (BasicBlock *bb : termInst->successors()) {
+        successors.push_back(&bb->front());
+      }
+    } else {
+      successors.push_back(inst->getNextNode());
+    }
+    for (Instruction *s : successors) {
+      for (StringRef n : live_in[s]) {
+        auto r = live_out[inst].insert(n);
+        if (r.second) {
+          errs() << "ADD OUT " << n << "\n";
+          changed = true;
+        }
+      }
+    }
+
+    /* live-in */
+    for (StringRef n : toGen) {
+      auto r = live_in[inst].insert(n);
+      if (r.second) {
+        errs() << "ADD IN " << n << "\n";
+      }
+    }
+    for (StringRef name : live_out[inst]) {
+      if (toKill.find(name) == toKill.end()) {
+        auto r = live_in[inst].insert(name);
+        if (r.second) {
+          errs() << "ADD IN " << name << "\n";
+          changed = true;
+        }
+      }
+    }
+
+    Instruction *prev = inst->getPrevNode();
+    if (prev) {
+      worklist.push_back(prev);
+    } else {
+      for (BasicBlock *pred : predecessors(inst->getParent())) {
+        Instruction *last = pred->getTerminator();
+        worklist.push_back(last);
+      }
+    }
+
+    visited.insert(inst);
+  }
+
+  return changed;
+}
+
+void LivenessAnalysis::gen(Instruction *inst,
+                           set<StringRef> &variables) {
+  for (unsigned i = 0; i < inst->getNumOperands(); i++) {
+    Value *v = inst->getOperand(i);
+    if (v->hasName()) {
+      variables.insert(v->getName());
+    }
+  }
+}
+
+void LivenessAnalysis::kill(Instruction *inst,
+                            set<StringRef> &variables) {
+  if (inst->getType()->isVoidTy()) {
+    return;
+  }
+
+  Value *v = (Value *)(inst);
+  if (v->hasName()) {
+    variables.insert(v->getName());
+  }
+}
+
+void LivenessAnalysis::dumpLiveSet(LiveSet &ls) {
+  errs() << "--- live set ---\n";
+  for (auto i : ls) {
+    errs() << "instruction: " << *i.first << "\n";
+    for (StringRef name : i.second) {
+      errs() << "-- live: " << name << "\n";
+    }
+  }
+}
+
+}
---
+++ lib/Core/LivenessAnalysis.h
@@ -0,0 +1,37 @@
+#ifndef LIVENESS_ANALYSIS_H
+#define LIVENESS_ANALYSIS_H
+
+#include <set>
+#include <map>
+
+#include <llvm/IR/Value.h>
+#include <llvm/IR/Instruction.h>
+#include <llvm/IR/Function.h>
+
+namespace klee {
+
+class LivenessAnalysis {
+
+public:
+
+  typedef std::map<llvm::Instruction *, std::set<llvm::StringRef>> LiveSet;
+
+  static void analyze(llvm::Function *f);
+
+  static bool runIteration(llvm::Function *f,
+                           LiveSet &live_in,
+                           LiveSet &live_out);
+
+  static void gen(llvm::Instruction *inst,
+                  std::set<llvm::StringRef> &variables);
+
+  static void kill(llvm::Instruction *inst,
+                   std::set<llvm::StringRef> &variables);
+
+  static void dumpLiveSet(LiveSet &ls);
+
+};
+
+}
+
+#endif
